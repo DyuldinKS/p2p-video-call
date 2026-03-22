46d96c48302f45920a93e6ca9b254953cb6bdbfb
Kirill Dyuldin <dyuldinks@gmail.com>
2026-03-23 00:44:47 +0100

    Expose store and peerSession to window in devMode

--- src/utils/callStore.ts
+++ src/utils/callStore.ts
@@ -8,3 +8,11 @@ interface CallStoreState {
 }
 
 export const [store, setStore] = createStore<CallStoreState>({});
+
+if (localStorage.getItem('devMode') === 'true') {
+  (window as any)._store = store;
+  Object.defineProperty(window, '_peer', {
+    get: () => store.peerSession,
+    configurable: true,
+  });
+}
